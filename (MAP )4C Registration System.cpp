7ee855dcb13620c3f6424ecfa50b118d23b2904b
gourav majee <44859494+varuogm@users.noreply.github.com>
2020-09-30 01:05:24 +0530

    Update (MAP )4C Registration System.cpp

--- (MAP )4C Registration System.cpp
+++ (MAP )4C Registration System.cpp
@@ -1,15 +1,30 @@
+INPUT
+4
+abacaba
+acaba
+abacaba
+acab
+
+OUTPUT
+
+OK
+OK
+abacaba1
+OK
+------------------------
+
+
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 
 int main()
 {
-	ios::sync_with_stdio(false);
+    ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
     int n;
     cin >> n;
-
     map< string , int> database;
     string s;
     while (n--)
