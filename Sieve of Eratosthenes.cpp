addfa42eedaf24a5ae0ed601df5d71c734c2c25c
gourav majee <44859494+varuogm@users.noreply.github.com>
2021-01-12 21:00:01 +0530

    Update Sieve of Eratosthenes.cpp

--- Sieve of Eratosthenes.cpp
+++ Sieve of Eratosthenes.cpp
@@ -1,4 +1,4 @@
-  ll maxN;
+  /*ll maxN;
 	maxN = 100000;
 	ll a[maxN+1];
 	for(ll i=2;i<=maxN;i++) 
@@ -12,3 +12,46 @@
 			}
 		}
 	}
+*/
+
+--------
+	
+#include<bits/stdc++.h>
+typedef long long ll;
+#define endl "\n"
+using namespace std;
+int ar[1001];
+int maxN = 1001;
+
+std::vector<int > v(maxN,1);//intialzed all with 1
+
+void seive(){
+  v[0]=0;v[1]=0; 
+  for(int i=2;i*i<=maxN;i++){
+    if(v[i]){
+      for(int  j=i*i;j<=maxN;j+=i){
+        v[j] = 0;
+      }
+    }
+  }
+}
+
+int main()
+{
+
+int t;
+cin>>t;
+while(t--)
+{
+int n;
+cin>>n;
+seive();
+
+if(v[n]==1)
+   cout<<"yes prime\n";
+else  
+  cout<<"nahi he prime\n";
+
+}
+return 0;
+}
